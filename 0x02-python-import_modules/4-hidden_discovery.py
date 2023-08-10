79c59f1271dc89703ec395954f083d90340a7edc
jeffmakuto <hjeffmakuto@proton.me>
2023-08-10 13:51:15 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print("{:name}".format(name))
