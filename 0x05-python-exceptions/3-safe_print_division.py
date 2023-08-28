757a661658eb13275614972f847aa5419e9678f4
jeffmakuto <hjeffmakuto@proton.me>
2023-08-28 14:23:45 +0300

    new files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except i(TypeError, ZeroDivisionError):
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
