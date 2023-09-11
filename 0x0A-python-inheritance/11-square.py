ac4743281e28fb2545ac51af0560f82e43747cf6
jeffmakuto <hjeffmakuto@proton.me>
2023-09-11 15:52:29 +0300

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,34 +1,53 @@
 #!/usr/bin/python3
-
 """
-Defines a Square class that inherits from Rectangle (9-rectangle.py).
+Class BaseGeometry and subclass Rectangle
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
+
+class BaseGeometry:
+    """public instance methods area and integer_validator"""
+    def area(self):
+        """raise exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value is an int > 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle"""
+    def __init__(self, width, height):
+        """instance of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """
-    Represents a square.
-    """
-
+    """A square"""
     def __init__(self, size):
-        """
-        Initializes a new Square instance.
-        Args:
-            size (int): The size of the square's sides.
-        """
+        """instance of the square"""
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
+
     def __str__(self):
-        """
-        Returns a string representation of the square.
-        """
-        return "[Square] {}/{}".format(self.__width, self.__height)
-
-
-if __name__ == "__main__":
-    s = Square(13)
-    print(s)
-    print(s.area())
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
